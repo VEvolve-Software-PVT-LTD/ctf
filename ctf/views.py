149fc57ac133d59c0fd5fe224fbea00da7b51c9a
Balu <balaraju.mme@gmail.com>
2019-08-07 15:23:56 +0530

    Added real time updates in team rankings

--- ctf/views.py
+++ ctf/views.py
@@ -29,7 +29,8 @@ from django.template import RequestContext
 from django.db.models import Sum
 from django_eventstream import get_current_event_id, send_event
 from django_eventstream.channelmanager import DefaultChannelManager
-
+from kafka import KafkaConsumer
+from kafka import KafkaProducer
 from ctf import models
 from ctf.forms import AnswerForm
 
@@ -38,6 +39,9 @@ redis_connection = get_redis_connection("default")
 
 # raw redis connection.
 r = redis.Redis()
+
+# kafka producer
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
 
 CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 
@@ -109,6 +113,10 @@ def get_teams(request):
 def get_questions_list(request):
     """ returns questions list."""
     key = 'vev/1278/ctf-questions'
+    user_key = '{}-key'.format(str(request.user.team))
+    print(user_key)
+    current_action = "Standing in Questions List"
+    r.set(user_key, current_action)
     if key in cache:
         questions = cache.get(key)
     else:
@@ -144,12 +152,20 @@ def get_question_detail(request,pk):
                 team_question.is_completed = True
                 team_question.save()                
                 # redirects to questions list on success.
+                data = "{} Answered  Question Number {}".format(str(request.user.team), str(team_question.question.id))
+                r.set('answer_action', data)
+                user_key = '{}-key'.format(request.user.team)
+                current_action = "Answered Question Number{}".format(question.id)
+                r.set(user_key, current_action)
                 return redirect('questions_list')
             messages.warning(request, _("Sorry wrong attempt"))
         # redirects to question detail page on wrong attempt.
         return redirect('question_detail', pk=question.pk)
     
     if request.method == 'GET':
+        user_key = '{}-key'.format(request.user.team)
+        current_action = "Standing in Question Number{}".format(question.id)
+        r.set(user_key, current_action)
         tq = models.TeamQuestion.objects.filter(
             question=question, 
             team=request.user.team).exists()
@@ -159,6 +175,8 @@ def get_question_detail(request,pk):
                 team=request.user.team
             )
             context['team_question'] = team_question
+            data = "{} Reopened Question Number {}".format(str(request.user.team), str(question.id))
+            r.set('question_action', data)
         else:
             team_question = models.TeamQuestion.objects.create(
                 team=request.user.team,
@@ -168,6 +186,8 @@ def get_question_detail(request,pk):
                 started_at = datetime.now()
             )
             context['team_question'] = team_question
+            data = "{} opened First time Question Number {}".format(str(request.user.team), str(question.id))
+            r.set('question_action', data)
             
         if question.question_has_clue:
             clues_count = models.Clue.objects.filter(question=question).count()
@@ -201,6 +221,9 @@ def get_clue(request, pk):
     """ Get  Clue View."""
     id=pk
     question_instance = models.Question.objects.get(id=id)
+    user_key = '{}-key'.format(request.user.team)
+    currect_action = "Taking Clue For Question Number{}".format(question_instance.id)
+    r.set(user_key, current_action)
     team_question = models.TeamQuestion.objects.get(
         question=question_instance, 
         team=request.user.team
@@ -246,6 +269,9 @@ class TeamDashBoard(DetailView):
         print(obj)
         if team != obj:
             return redirect('team_dash_board', pk=team.id)
+        user_key = '{}-key'.format(request.user.team)
+        current_action = "Watching Score Board"
+        r.set(user_key, current_action)
         return super(TeamDashBoard, self).dispatch(request, *args, **kwargs)
         
     def get_object(self, queryset=None):
@@ -287,25 +313,89 @@ def team_rankings(request):
     context=  {}
     context['url'] = '/events/'
     context['last_id'] = get_current_event_id(['time'])
+    context['question_id'] = get_current_event_id(['begin'])
     scores = models.TeamQuestion.objects.filter(is_completed=True)
     team_scores = scores.values('team__team_name').annotate(count=Sum('gain_points'))
     context['team_scores'] = team_scores
     return render(request, 
-                  'team_scores.html', 
+                  'event.html', 
                   context
                  )
 
 
+import random
 import json
+
 def _send_worker():
     while True:
+        question_action = r.get('question_action').decode('utf-8')
+        answer_action = r.get('answer_action').decode('utf-8')
         scores = models.TeamQuestion.objects.filter(is_completed=True)
-        team_scores = list(scores.values('team__team_name').annotate(count=Sum('gain_points')))
+        team_scores = list(scores.values('team__team_name').annotate(count=Sum('gain_points')).order_by('-count'))
         data = json.dumps(team_scores)
-        send_event('time', 'message', json.dumps(team_scores))
+        _table_start = '<div class="container"><div class="row"></div> <div class="col-lg-4"><table class="table"><thead class="bg-primary"><tr><td>Team</td><td>Score</td></tr></thead>'
+        _body_data = '<tr><td>{}</td><td>{}</td></tr>'
+        _table_end = '</tr></thead></table></div>'
+        for i in range(len(team_scores)):
+            instance = '<tr><td>{}</td><td>{}</td></tr>'.format(team_scores[i]['team__team_name'], team_scores[i]['count'])
+            _table_start = _table_start + instance
+        _table_start = _table_start + _table_end
+       
+        _html_data = '<div class="container" style="margin-top:5%;"> <div class="row"><div class="col-md-4">'
+        _table_start = '<table class="table"><thead class="bg-primary"><tr><td>Team</td><td>Score</td></tr></thead>'
+        for i in range(len(team_scores)):
+            instance = '<tr><td>{}</td><td>{}</td></tr>'.format(team_scores[i]['team__team_name'], team_scores[i]['count'])
+            _table_start = _table_start + instance
+        _table_end = '</tr></thead></table></div>'
+        _table_start = _table_start + _table_end
+        _html_data = _html_data+_table_start
+        
+#        _top_three_data = '<div class="col-lg-4"><div class="card">'
+        _top_three_data = '<div class="col-lg-4"><div class="row">'
+        _top_three_end = '</div></div>'
+        for i in range(3):
+            inner_data = '<div class="col-lg-12 col-md-12" style="margin-top:2%;"></div>'
+            
+            _top_three_data = _top_three_data + inner_data
+        _top_three_data = _top_three_data+_top_three_end        
+        _html_data = _html_data + _top_three_data
+        
+        
+        _actions_data = '<div class="col-lg-4 col-md-4">'
+        _inner_action_data1 = '<div class="e-alert snack sky ePull" style="margin-top:2%;"><h4>{}</h4></div>'.format(question_action)
+        _inner_action_data2 = '<div class="e-alert snack success" style="margin-top:2%;"><h4>{}</h4></div>'.format(answer_action)
+        _actions_end = '</div>'
+        _actions_data = _actions_data+_inner_action_data1+_inner_action_data2+_actions_end
+        _html_data = _html_data + _actions_data
+        
+        _second_section = '<div class="container"><div class="row">'
+        for i in range(len(team_scores)):
+            _team_action_key = team_scores[i]['team__team_name']+'-key'
+            if r.exists(_team_action_key):                
+                _team_action = r.get(_team_action_key).decode('utf-8')
+            else:
+                _team_action = 'No Actions Yet.'
+            _team_data_start = '<div class="col-lg-4 col-md-4 e-card" style="margin:1%;"><div cass="e-card">\
+                                <div class="card-body"><h5 class="card-title text-primary">{}</h5><h6>action: {}</h6>\
+                                <a class="e-btn purple inverted">Details</a>&emsp;&emsp;&emsp;&emsp;\
+                                <a class="e-btn danger inverted danger">Points:<span class="text-primary"> {}</span></a></div></div></div>'.format(team_scores[i]['team__team_name'], _team_action, team_scores[i]['count'])
+            
+            
+            _second_section = _second_section + _team_data_start
+        _second_section_end = '</div></div>'
+        _second_section = _second_section + _second_section_end
+        
+        total_page = _html_data + _second_section
+        send_event('time', 'message',total_page)
         time.sleep(1)
     
-    
+
+def _send_worker2():
+    while True:
+        data = 'lionis started question 1'
+        send_event('begin', 'question', data)
+        time.sleep(1)
+        
 def _db_ready():
     from django.db import DatabaseError
     from django_eventstream.models import Event
@@ -317,6 +407,7 @@ def _db_ready():
         return False
 
 if _db_ready():
+    threds = []
     send_thread = threading.Thread(target=_send_worker)
     send_thread.daemon = True
     send_thread.start()
@@ -336,3 +427,5 @@ def answer_question(request):
         else:
             message.warning(request, _("Sorry Wrong Attempt."))
     return redirect('question_detail', pk=instance_question.id)
+
+
